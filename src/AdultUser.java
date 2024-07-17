20d8b8a0b01d05d37461625a30d4a94fe0aaabf2
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-07-17 09:13:52 -0400

    Created a Adult class with  age and bookType variables

---
+++ src/AdultUser.java
@@ -0,0 +1,34 @@
+public class AdultUser implements LibraryUser {
+    private int age;
+    private String bookType;
+
+    public AdultUser() {
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setBookType(String bookType) {
+        this.bookType = bookType;
+    }
+
+    @Override
+    public void registerAccount() {
+        this.age = age;
+        if (age >= 11) {
+            System.out.println("You have successfully registered under an Adult Account");
+        } else {
+            System.out.println("Sorry, age must be greater than 12 to register as an adult");
+        }
+
+    }
+
+    public void requestBook() {
+        if (bookType.equals("Fiction")) {
+            System.out.println("Book issued successfully, please return the book with 7 days");
+        } else {
+            System.out.println("Opps, you are allowed to take only adult Fiction books");
+        }
+    }
+}
